98acb248cf218caedc8c85766cd3db53ef8ab262
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-06-16 12:15:18 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        char arr[26] = {0};
+
+        for (auto it: s){
+            arr[it - 'a'] += 1;
+        }
+        for (auto it: t){
+            arr[it - 'a'] -= 1;
+        }
+
+        for ( int i = 0; i < 26; i++){
+            if (arr[i] > 0) return false;
+        }
+        return true;
+
+
+        
+    }
+};
