5bb02b7ba376844a8d07be4db75576c442135a12
Shiva G. Hiremath <153100208+shivahiremath001@users.noreply.github.com>
2026-06-18 09:41:02 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+
+        while True:
+            z = target - nums[i]
+
+            if z in nums and nums.index(z) != i:
+                return sorted([i, nums.index(z)])
+
+            i += 1
+        
+        if i >= len(nums):
+            return -1
